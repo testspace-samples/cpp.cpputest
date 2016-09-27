fdffc3022d9d47e6591a18c3b0ff0fca070222ae
Ivailo Petrov <ivailop@s2technologies.com>
2016-09-27 21:35:57 +0000

    cleaned up, updated to cpputest-3.8, and added build.log publishing

--- .c9build.sh
+++ .c9build.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
 # System prerequisites 
-sudo apt-get install wget unzip python make g++
-sudo apt-get install automake autoconf libtool
+#sudo apt-get install wget unzip python make g++
+#sudo apt-get install automake autoconf libtool
 sudo pip install gcovr 
 
 # Download framework
-rm -rf cpputest
-wget --no-check-certificate -O cpputest-3.7.1.zip https://github.com/cpputest/cpputest.github.io/blob/master/releases/cpputest-3.7.1.zip?raw=true
-unzip cpputest-3.7.1.zip
-mv cpputest-3.7.1 cpputest
-rm cpputest-3.7.1.zip
+CPPUTEST_HOME=$PWD/cpputest-3.8
+rm -rf $CPPUTEST_HOME
+wget https://github.com/cpputest/cpputest/releases/download/v3.8/cpputest-3.8.zip
+unzip cpputest-3.8.zip
+rm cpputest-3.8.zip
 
-# Build (i) library (ii) examples
-cd cpputest/cpputest_build
+# Build library
+pushd $CPPUTEST_HOME/cpputest_build
 autoreconf .. -i
 ../configure
-make
+make SILENCE=@
 cp -R ./lib ../
+popd
 
-cd ../examples
-make CPPUTEST_USE_GCOV=Y clean all_no_tests 
+# Build examples
+rm -f build.log
+# (i) building with coverage (ii) preserving STDOUT while saving to a log file (iii) preserving the original exit code
+make -C $CPPUTEST_HOME/examples CPPUTEST_USE_GCOV=Y clean all_no_tests |& tee build.log ; test ${PIPESTATUS[0]} -eq 0
 
 # Analysis
-
+# using the build.log
 
 # Test
-rm -rf cpputest_*.xml
-./CppUTestExamples_tests -o junit -v
-gcovr --root ../.. --filter ".*examples.*" --exclude ".*AllTests.*" -x -o coverage.xml
+rm -f cpputest_*.xml
+$CPPUTEST_HOME/examples/CppUTestExamples_tests -o junit -v
+rm -f coverage.xml
+gcovr --root ./ --filter ".*examples/ApplicationLib/.*" -x -o coverage.xml
 
 
 # Publish
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux.tgz | sudo tar -zxvf- -C /usr/local/bin
-testspace [Tests]cpputest_*.xml coverage.xml master.c9
+# assuming testspace is already configured with "credentials@domain/project"
+#testspace build.log{issues} [Tests]cpputest_*.xml coverage.xml master.c9
