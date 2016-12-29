b3fd104cf304c171bfd1021916ad383badb4efea
Mark Underseth <marku@s2technologies.com>
2016-12-29 18:58:13 +0000

    updated to connected project

--- .c9build.sh
+++ .c9build.sh
@@ -34,8 +34,14 @@ $CPPUTEST_HOME/examples/CppUTestExamples_tests -o junit -v
 rm -f coverage.xml
 gcovr --root ./ --filter ".*examples/ApplicationLib/.*" -x -o coverage.xml
 
+# Push content
 
-# Publish
+## Requires TESTSPACE_TOKEN = $ACCESS_TOKEN:@samples.testspace.com. 
+
+BRANCH_NAME=`git symbolic-ref --short HEAD`
+GIT_URL=`git remote show origin -n | grep Fetch\ URL: | sed 's/.*URL: //'`
+REPO_SLUG=`echo ${GIT_URL#*github.com?} | sed 's/.git//'`
+
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux.tgz | sudo tar -zxvf- -C /usr/local/bin
-# assuming testspace is already configured with "credentials@domain/project"
-testspace build.log{issues} [Tests]cpputest_*.xml coverage.xml master.c9
+testspace @.testspace.txt $TESTSPACE_TOKEN/${REPO_SLUG/\//:}/${BRANCH_NAME}#c9.Build
+
