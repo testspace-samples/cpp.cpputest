6af1244c9f281d662cd2606b7dc31a16877de316
Mark Underseth <marku@s2technologies.com>
2017-06-22 15:12:40 +0000

    use standard curl download options

--- .c9build.sh
+++ .c9build.sh
@@ -36,7 +36,7 @@ gcovr --root ./ --filter ".*/ApplicationLib/.*" -x -o coverage.xml
 
 # Download and configure the Testspace client
 mkdir -p $HOME/bin
-curl -s https://testspace-client.s3.amazonaws.com/testspace-linux.tgz | tar -zxvf- -C $HOME/bin
+curl -fsSL https://testspace-client.s3.amazonaws.com/testspace-linux.tgz | tar -zxvf- -C $HOME/bin
 CI=true testspace config url samples.testspace.com
 testspace -v
 
